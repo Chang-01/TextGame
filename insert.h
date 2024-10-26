0e056f3e4c9bccbd962b04c852a6ad5e3cef1ec1
Chang-01 <ltt100@naver.com>
2024-10-26 13:20:27 +0900

    Add files via upload

---
+++ insert.h
@@ -0,0 +1,11 @@
+#pragma once
+
+typedef struct property {
+	int hp, ad, df;
+} pro;
+
+typedef struct info {
+	char id[20];
+	char pw[20];
+	char name[10];
+}info;
---
+++ story.c
@@ -0,0 +1,21 @@
+
+#define _CRT_SECURE_NO_WARNINGS
+#include "insert.h"
+#include <stdio.h>
+
+
+int main(void) {
+	int a;
+
+	printf("게임을 시작합니다.\n 새로시작하기 : 1\n 불러오기 : 2\n");
+	scanf("%d", &a);
+	if (a = 1) {
+
+	}
+	else if (a = 2) {
+
+	}
+	else {
+		printf("잘못된 입력입니다.");
+	}
+}
